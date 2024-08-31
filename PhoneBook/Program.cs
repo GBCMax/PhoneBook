fb8a7b5e897cca8aeb776dd5ef9145a7b7292b65
max_on <maks_on_max@mail.ru>
2024-08-31 14:09:43 +0700

    Добавьте файлы проекта.

---
+++ PhoneBook/Program.cs
@@ -0,0 +1,43 @@
+﻿using PhoneBook;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    var phoneBook = new List<Contact>();
+
+    phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+    phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+    phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+    phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+    phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+    phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+    phoneBook.Add(new Contact("Максим", "Иванов", 79843453743, "maks@example.com"));
+    phoneBook.Add(new Contact("Криштиано", "Рональдо", 79843453743, "ronaldo@example.com"));
+    phoneBook.Add(new Contact("Алексей", "Никитин", 78347663482742, "alexey@example.com"));
+    phoneBook.Add(new Contact("Артем", "Дзюба", 435634895345, "dzyuba@example.com"));
+    phoneBook.Add(new Contact("Лев", "Яшин", 435634895345, "dzyuba@example.com"));
+
+    Console.WriteLine("Неотсортированный список:");
+
+    Console.WriteLine();
+
+    foreach(var contact in phoneBook)
+    {
+      Console.WriteLine($"Имя: {contact.Name}, фамилия: {contact.LastName}");
+    }
+
+    Console.WriteLine();
+
+    phoneBook = [.. phoneBook.OrderBy(c => c.Name).ThenBy(c => c.LastName)];
+
+    Console.WriteLine("Отсортированный список:");
+
+    Console.WriteLine();
+
+    foreach (var contact in phoneBook)
+    {
+      Console.WriteLine($"Имя: {contact.Name}, фамилия: {contact.LastName}");
+    }
+  }
+}
